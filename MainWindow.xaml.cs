79d2eb9a92a91770fe0865f3f3b2913295cb89bb
omerhijazi404 <omerhijazi@outlook.com>
2020-09-22 08:02:03 +0300

    Initialization

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Threading.Tasks;
+
+namespace TextEditWPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+        }
+
+    }
+}
---
+++ SplashScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace TextEditWPF {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            Splash();
+        }
+        
+        private async void Splash() {
+            Storyboard FadeSplashScreen = FindResource("MakeSmaller") as Storyboard;
+            FadeSplashScreen.Completed += StartMain;
+            await Task.Delay(2000);
+            FadeSplashScreen.Begin();
+        }
+
+        private void CloseW(object sender, RoutedEventArgs e) {
+            Close();
+        }
+        private void MinimizeW(object sender, RoutedEventArgs e) {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void StartMain(object sender, EventArgs e) {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
